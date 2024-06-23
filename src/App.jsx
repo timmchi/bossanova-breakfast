0ee5961e26d7ded77955d2da252ca2f0834ac8a6
timmchi <tnmagg@gmail.com>
2024-06-23 15:43:10 +0400

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import StationList from "./components/StationList";
 function App() {
   return (
     <div>
+      <div className="main-text">
+      <h1 className="site-title">Bossa nova breakfast</h1>
+      <h3 className="slogan"><i>Choose your soundtrack, then choose your breakfast</i></h3>
+      </div>
       <StationList />
     </div>
   );
